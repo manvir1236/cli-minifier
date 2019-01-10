6f5cf3d9feff4265c29d40721b906834f719bae0
unknown <manvir.singh@WKS175.nsi.lab>
2019-01-10 15:00:34 +0530

    Autoload Path Changed

--- process.php
+++ process.php
@@ -1,5 +1,9 @@
 <?php
-require 'vendor/autoload.php';
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require __DIR__.'/../vendor/autoload.php';
+} else {
+    require __DIR__.'/../../../autoload.php';
+}
 use MatthiasMullie\Minify;
 $app = new Silly\Application();
 $app->command('js [filename]', function ($filename) {
